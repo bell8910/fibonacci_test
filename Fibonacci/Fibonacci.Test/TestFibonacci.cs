4ed6ab40bf2f51ce10c40d98cfa8ef590e8fe2c4
bell <1993430@qq.com>
2019-03-17 18:09:03 +0800

    fibo test pass

--- Fibonacci/Fibonacci.Test/TestFibonacci.cs
+++ Fibonacci/Fibonacci.Test/TestFibonacci.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Fibonacci.Test
@@ -36,6 +37,34 @@ namespace Fibonacci.Test
             Assert.AreEqual(res2, (123497 * 9876).ToString());
 
         }
+
+
+        [TestMethod]
+        public void TestMatrixPower()
+        {
+            var x = Fibonacci.FiboMatrix;
+            var y = new string[2, 2] { { "1", "0" }, { "0", "1" } };
+          //  var z = Fibonacci.Matrix_mul(x, y);
+           string value = null;
+            /*
+           var n2 = Fibonacci.Get_FirstN_By_Matrix_power(2,out value);
+            Debug.WriteLine("n={0},value={1}",n2,value);
+            Assert.AreEqual(n2,7);
+            Assert.AreEqual(value, "13");
+
+            var n3 = Fibonacci.Get_FirstN_By_Matrix_power(3,out value);
+            Debug.WriteLine("n={0},value={1}", n3, value);
+            Assert.AreEqual(n3,12);
+            Assert.AreEqual(value, "144");
+            */
+
+           // var start = Fibonacci.Get_LastN_By_Matrix_power(1000);
+            // var result = Fibonacci.Check_N_By_BinarySearch(4096);
+            var n1000 = Fibonacci.Get_FirstN_By_Matrix_power(1000, out value);
+            Debug.WriteLine("n={0},value={1}", n1000, value);
+        }
+
+
 
 
     }
--- Fibonacci/Fibonacci/Program.cs
+++ Fibonacci/Fibonacci/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -7,59 +8,16 @@ namespace Fibonacci
 {
     class Program
     {
-        static int[,] FiboMatrix = new int[2, 2] {{1, 1}, {1, 0}};
-
-
-        //矩阵乘法
-        static int[,] Matrix_mul(int[,] x,int[,] y) 
-         {  
-            int i,j,k;
-            var tmp = new int[2, 2] {{0, 0}, {0, 0}};
-            for(i=0; i<2; i++)  
-                for(j=0; j<2; j++)  
-                    for(k=0; k<2; k++)
-                        tmp[i,j] += x[i,k] * y[k,j];  
-            return tmp;  
-        }
-
-        //输出矩阵结果
-        static void OutputMatrix(int[,] x)
-        {
-           for(var i=0;i<2;i++)
-           {
-               for(var j=0;j<2;j++)
-                   Console.Write(x[i,j] + " ");
-               Console.WriteLine();
-           }
-              
-        }
-
-        //矩阵快速求幂
-        static int[,] Matrix_power(int[,] x, int p)
-        {
-            var y = new int[2, 2] {{1, 0}, {0, 1}};
-
-            while (p > 0)
-            {
-                if ((p & 1) != 0)
-                    y = Matrix_mul(y,x);
-                x = Matrix_mul(x, x);
-                p >>= 1;
-            }
-            return y;
-        }
-
+        
         static void Main(string[] args)
         {
-            /* var result =Matrix_power(FiboMatrix,10);
-             OutputMatrix(result);
-
-             var bigResult =BigInteger.Add("123", "389");
-               Console.WriteLine("big number={0}",bigResult);
-              
-             */
-            var mul = Karatsuba.karatsuba("12323213213", "1232131231231");
-            Console.WriteLine(mul);
+            var s = new Stopwatch();
+            s.Start();
+            string value = null;
+            var n1000 = Fibonacci.Get_FirstN_By_Matrix_power(1000, out value);
+            Console.WriteLine("n={0},value={1}", n1000, value);
+            s.Stop();
+            Console.WriteLine("time consume=" + s.Elapsed);
             Console.ReadKey();
 
 
